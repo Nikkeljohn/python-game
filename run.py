e4901d2b1b484e1d24e34b9a3374d68a3edfba4e
Nikkeljohn <nikkeljohn120@gmail.com>
2022-12-18 21:13:26 +0000

    saved and commit old line

--- run.py
+++ run.py
@@ -14,3 +14,19 @@ class colors:
     orange = '\033[33m'
     white = '\033[0m'
     bold = '\033[1m'
+
+def player_name():
+    """
+    player has enter their name . 
+    This will be used in game so their name will be used,
+     in the game to wish them luck
+     """
+     global name
+     while True:
+         name = input("\nWho is playing ?")
+         if name.isalpha():
+             break
+        print(colors.green + "Valid letters (A-Z)only please.\n"  + colors.white)
+     sleep(1)
+     print("\nGood luck to you," + colors.red + f"{name.capitalize()}!")
+     return name       
