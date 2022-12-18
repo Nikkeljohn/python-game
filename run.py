3ae7697bc39d654a1e1d5726659d4a8e9d3a4ae1
Nikkeljohn <nikkeljohn120@gmail.com>
2022-12-18 20:56:28 +0000

    added colors for the game

--- run.py
+++ run.py
@@ -1,3 +1,16 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+
+import random
+from words import words
+from hungman import lives_left
+import string
+from time import sleep
+
+#colors for the game 
+class colors:
+    purple = '\033[95m'
+    cyan = '\033[96m'
+    green = '\033[92m'
+    red = '\033[91m'
+    orange = '\033[33m'
+    white = '\033[0m'
+    bold = '\033[1m'
